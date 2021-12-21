1757fcbaa11a5556d7c73b76ba9f62b03df06d32
SHan <hs04111@naver.com>
2021-12-21 11:47:32 +0900

    Trello: basic setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
-import React from 'react';
-import styled from 'styled-components';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 function App() {
-  return <div></div>;
+  const onDragEnd = () => {};
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <Droppable droppableId="one">
+          {(provided) => (
+            <ul ref={provided.innerRef} {...provided.droppableProps}>
+              <Draggable draggableId="first" index={0}>
+                {(provided) => (
+                  <>
+                    <li ref={provided.innerRef} {...provided.draggableProps}>
+                      One<span {...provided.dragHandleProps}>handle</span>
+                    </li>
+                  </>
+                )}
+              </Draggable>
+              <Draggable draggableId="second" index={1}>
+                {(provided) => (
+                  <li
+                    ref={provided.innerRef}
+                    {...provided.dragHandleProps}
+                    {...provided.draggableProps}
+                  >
+                    Two
+                  </li>
+                )}
+              </Draggable>
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
+  );
 }
 
 export default App;
