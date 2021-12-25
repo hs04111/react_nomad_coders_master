d91afd2a252f3ee4d87899b60aee83b4119a15dd
SHan <hs04111@naver.com>
2021-12-25 14:31:49 +0900

    Trello clone: multi boards

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import {
-  DragDropContext,
-  Droppable,
-  Draggable,
-  DropResult,
-} from 'react-beautiful-dnd';
+import { DragDropContext, DropResult } from 'react-beautiful-dnd';
 import { useRecoilState } from 'recoil';
-
 import styled from 'styled-components';
 import { toDoAtom } from './atoms';
-import DraggableCard from './components/DraggableCard';
+import Board from './components/Board';
 
 const Wrapper = styled.div`
   display: flex;
-  max-width: 480px;
+  max-width: 680px;
   width: 100%;
   margin: 0 auto;
   justify-content: center;
@@ -23,15 +17,8 @@ const Wrapper = styled.div`
 const Boards = styled.div`
   display: grid;
   width: 100%;
-  grid-template-columns: repeat(1, 1fr);
-`;
-
-const Board = styled.div`
-  padding: 20px 10px;
-  padding-top: 30px;
-  background-color: ${(props) => props.theme.boardColor};
-  border-radius: 5px;
-  min-height: 200px;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
 `;
 
 function App() {
@@ -39,27 +26,20 @@ function App() {
 
   const onDragEnd = ({ destination, source, draggableId }: DropResult) => {
     if (!destination) return;
-    setToDos((prev) => {
-      const arr = [...prev];
-      arr.splice(source.index, 1);
-      arr.splice(destination?.index, 0, draggableId);
-      return arr;
-    });
+    // setToDos((prev) => {
+    //   const arr = [...prev];
+    //   arr.splice(source.index, 1);
+    //   arr.splice(destination?.index, 0, draggableId);
+    //   return arr;
+    // });
   };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
         <Boards>
-          <Droppable droppableId="one">
-            {(magic) => (
-              <Board ref={magic.innerRef} {...magic.droppableProps}>
-                {toDos.map((toDo, index) => (
-                  <DraggableCard key={toDo} index={index} toDo={toDo} />
-                ))}
-                {magic.placeholder}
-              </Board>
-            )}
-          </Droppable>
+          {Object.keys(toDos).map((boardId) => (
+            <Board key={boardId} boardId={boardId} toDos={toDos[boardId]} />
+          ))}
         </Boards>
       </Wrapper>
     </DragDropContext>
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,6 +1,15 @@
 import { atom, selector } from 'recoil';
 
-export const toDoAtom = atom({
+interface IToDoState {
+  [key: string]: string[];
+}
+// 이렇게 작성함으로써, atom에 현재 지정된 key 뿐만 아니라 다른 string으로 이루어진 key가 있을 가능성을 제시
+
+export const toDoAtom = atom<IToDoState>({
   key: 'toDos',
-  default: ['a', 'b', 'c', 'd', 'e', 'f'],
+  default: {
+    to_do: ['a'],
+    doing: ['c', 'd'],
+    done: ['b'],
+  },
 });
