83572fe6a7773bc8b3db45157226d419b7a596e4
SHan <hs04111@naver.com>
2021-12-25 23:47:39 +0900

    Framer motion: variants and ochestration

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,57 @@ const Wrapper = styled.div`
 `;
 
 const Box = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
   width: 200px;
   height: 200px;
-  background-color: white;
+  background-color: rgba(255, 255, 255, 0.2);
   border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
+
+const Circle = styled(motion.div)`
+  place-self: center;
+  width: 70px;
+  height: 70px;
+  border-radius: 35px;
+  background-color: white;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  start: {
+    scale: 0.5,
+    opacity: 0.5,
+  },
+  end: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      duration: 0.5,
+      bounce: 0.5,
+      staggerChildren: 0.2,
+      // https://www.framer.com/docs/transition/###staggerchildren
+    },
+  },
+};
+
+const circleVariants = {
+  start: { opacity: 0, y: 10 },
+  end: { opacity: 1, y: 0 },
+};
+
 function App() {
   return (
     <Wrapper>
-      <Box
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, borderRadius: '50px', rotateZ: 360 }}
-        transition={{ type: 'spring', damping: 5 }}
-        // transition docs
-        // https://www.framer.com/docs/transition
-      />
+      <Box variants={boxVariants} initial="start" animate="end">
+        {/* 부모에게 initial과 animate를 주면 자식에게도 똑같은 이름으로 주어진다 */}
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
