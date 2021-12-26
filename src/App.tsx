5b644b8197288f9b0806ed66a83409b7edfeea7d
SHan <hs04111@naver.com>
2021-12-26 11:11:11 +0900

    Framer motion: gestures part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion';
 import styled from 'styled-components';
+
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
@@ -13,53 +14,34 @@ const Box = styled(motion.div)`
   grid-template-columns: repeat(2, 1fr);
   width: 200px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: white;
   border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  place-self: center;
-  width: 70px;
-  height: 70px;
-  border-radius: 35px;
-  background-color: white;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
 const boxVariants = {
-  start: {
-    scale: 0.5,
-    opacity: 0.5,
-  },
-  end: {
-    scale: 1,
-    opacity: 1,
+  click: { scale: 0.7, borderRadius: '100px' },
+  hover: { rotateZ: 90, scale: 1.5 },
+  drag: {
+    backgroundColor: '#173438',
     transition: {
-      type: 'spring',
-      duration: 0.5,
-      bounce: 0.5,
-      staggerChildren: 0.2,
-      // https://www.framer.com/docs/transition/###staggerchildren
+      duration: 3,
     },
   },
-};
-
-const circleVariants = {
-  start: { opacity: 0, y: 10 },
-  end: { opacity: 1, y: 0 },
+  // color를 animate하려면 문자열로 된 ('blue') 색 말고 헥사코드나 rgb를 사용할것
+  // 숫자여야 한다
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial="start" animate="end">
-        {/* 부모에게 initial과 animate를 주면 자식에게도 똑같은 이름으로 주어진다 */}
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
+      <Box
+        drag
+        whileDrag="drag"
+        variants={boxVariants}
+        whileTap="click"
+        whileHover="hover"
+      />
     </Wrapper>
   );
 }
