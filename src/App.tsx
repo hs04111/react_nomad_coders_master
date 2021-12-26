f0d9c3ff8258e344206897b3204a159b8ee6ebd6
SHan <hs04111@naver.com>
2021-12-26 14:21:03 +0900

    Framer motion: useMotionValue part3

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { motion, useMotionValue, useTransform } from 'framer-motion';
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  useViewportScroll,
+} from 'framer-motion';
 import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -22,9 +27,23 @@ const boxVariants = {};
 
 function App() {
   const x = useMotionValue(0);
+
   // x는 계속 추적되지만, 값이 바뀐다고 re-rendering이 일어나지는 않는다.
   // 계속 렌더링되면 퍼포먼스 측면에서 좋지 않다
-  const scale = useTransform(x, [-800, 0, 800], [10, 5, 0.1]);
+  // https://www.framer.com/docs/motionvalue/
+  const rotateZ = useTransform(x, [-800, 800], [-360, 720]);
+  const gradient = useTransform(
+    x,
+    [-800, 0, 800],
+    [
+      'linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))',
+      'linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))',
+      'linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))',
+    ],
+  );
+  const { scrollYProgress } = useViewportScroll();
+  // https://www.framer.com/docs/motionvalue/##useviewportscroll
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
   useEffect(() => {
     x.onChange(() => {
       console.log(x.get());
@@ -32,10 +51,8 @@ function App() {
   }, [x]);
   // useEffect 내부의 get(), set(), onChange() 함수들은 Framer motion에서 온 함수들이다.
   return (
-    <Wrapper>
-      {/* https://www.framer.com/docs/gestures/#drag */}
-
-      <Box drag="x" style={{ x: x, scale: scale }} dragSnapToOrigin />
+    <Wrapper style={{ background: gradient }}>
+      <Box drag="x" style={{ x: x, rotateZ, scale }} dragSnapToOrigin />
     </Wrapper>
   );
 }
